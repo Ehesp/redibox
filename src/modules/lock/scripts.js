0c6bcd81f6ba646722d9981ba03caafa7f9ab496
Salakar <mike.diarmid@gmail.com>
2016-01-12 21:15:02 +0000

    moved from other github org to personal repo

--- src/modules/lock/scripts.js
+++ src/modules/lock/scripts.js
@@ -2,7 +2,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 Invertase
+ * Copyright (c) 2015 Salakar
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
