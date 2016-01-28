ca4be26064495c60738dd8f577c7d694c3eb720e
Salakar <mike.diarmid@gmail.com>
2016-01-28 18:57:09 +0000

    job .onSuccess and .onFailure events from anywhere - via sockets ;)

--- src/modules/job/job.js
+++ src/modules/job/job.js
@@ -25,11 +25,13 @@
  */
 
 import {noop, sha1sum} from './../../helpers';
+import cuid from 'cuid';
 
 class Job {
 
   constructor(queue, jobId, data = {}, options = {
-    unique: false
+    unique: false,
+    timeout: 60000 // 1 minute default timeout
   }) {
     this.queue = queue;
     this.rdb = this.queue.rdb;
@@ -38,6 +40,7 @@ class Job {
     this.progress = 0;
     this.data = data;
     this.options = options;
+    this.subscriptions = [];
     this.status = 'created';
   }
 
@@ -59,33 +62,89 @@ class Job {
 
   toData() {
     return JSON.stringify({
+      id: this.id,
       data: this.data,
       options: this.options,
       status: this.status
     });
   }
 
-  save(cb = noop) {
-    this.rdb.log.verbose(`Saving new job for ${this.queue.name}`);
+  /**
+   * Internal saver.
+   * @param cb
+   * @private
+   */
+  _save(cb) {
+    this.rdb.log.verbose(`Saving new job ${this.id} for ${this.queue.name}`);
     this.rdb.client.addjob(
       this.queue.toKey('jobs'),
       this.queue.toKey('waiting'),
       this.queue.toKey('id'),
       this.toData(),
       !!this.options.unique,
-      this.options.unique ? sha1sum(this.data) : '', (err, jobId) => {
+      this.id, (err, id) => {
         this.rdb.log.verbose(`Saved job for ${this.queue.name}`);
-        if (jobId === 0 && this.options.unique) {
+        if (id === 0 && this.options.unique) {
           this.duplicate = true;
-          return cb();
+          return cb(new Error('Duplicate Job')); // TODO improve on this error message.
         }
         if (err) return cb(err);
-        this.id = jobId.toString();
+        this.id = id;
         this.status = 'saved';
-        this.queue.jobs[jobId] = this;
+        this.queue.jobs[id] = this;
         return cb(null, this);
       }
     );
+  }
+
+  save(cb = noop) {
+    this.id = this.queue.name + '-' + (this.options.unique ? sha1sum(this.data) : cuid());
+
+    if (this.options.notifySuccess) {
+      this.options.notifySuccess = `job:${this.id}:success`;
+      this.subscriptions.push(`job:${this.id}:success`);
+    }
+
+    if (this.options.notifyFailure) {
+      this.options.notifyFailure = `job:${this.id}:failure`;
+      this.subscriptions.push(`job:${this.id}:failure`);
+    }
+
+    if (this.options.notifySuccess || this.options.notifyFailure) {
+      return this.rdb.subscribeOnceOf(
+        this.subscriptions, (message) => { // on message received
+          const channel = message.channel;
+
+          // remove the pubsub data
+          if (message.data) {
+            message = message.data;
+          }
+
+          // if there's an error the assume failed.
+          if (message.error) {
+            return this.onFailureCallback(message);
+          }
+
+          // is it from the success channel.
+          if (this.subscriptions[0] === channel) {
+            return this.onSuccessCallback(message);
+          }
+
+          return this.onFailureCallback(message);
+        }, (err) => { // subscribed callback
+          if (err) {
+            this.onFailureCallback({
+              type: 'job',
+              error: new Error('Error while subscribing to job events, however this job will still be queued - ' +
+                'you may be unable to receive onComplete / onFailure events for this job.')
+            });
+          }
+          this._save(cb);
+          // adding a additional 2s onto the subscribe timeout to allow timeout
+          // on the job to emit it's own timeout event.
+        }, this.options.timeout + 2000);
+    }
+    this._save(cb);
     return this;
   }
 
@@ -97,9 +156,18 @@ class Job {
     return this;
   }
 
-  // TODO
-  onComplete(completionCb) {
-    this.completionCb = completionCb;
+  onSuccess(notify) {
+    this.options.notifySuccess = true;
+    this.onSuccessCallback = notify;
+    if (!this.onFailureCallback) this.onFailureCallback = noop;
+    return this;
+  }
+
+  onFailure(notify) {
+    this.options.notifyFailure = true;
+    this.onFailureCallback = notify;
+    if (!this.onSuccessCallback) this.onSuccessCallback = noop;
+    return this;
   }
 
   unique(bool) {
@@ -109,7 +177,7 @@ class Job {
 
   timeout(ms) {
     if (ms < 0) {
-      throw Error('Timeout cannot be negative');
+      throw Error('Timeout cannot be negative.'); // TODO improve error message.
     }
     this.options.timeout = ms;
     return this;
@@ -128,6 +196,14 @@ class Job {
       event: 'progress',
       data: progress
     }), cb);
+  }
+
+  initialJob() {
+    return this._internalData.initialJob;
+  }
+
+  initialQueue() {
+    return this._internalData.initialQueue;
   }
 
   remove(cb = noop) {
